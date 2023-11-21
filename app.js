8f62d8a66670980435420f12dcce950c298dae73
“Ankit <22amtics124@gmail.com>
2023-11-21 17:17:08 +0530

    added external apios

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const path = require("path")
 const ProductDetails = require("./models/productDetails");
 const addProductHandler = require("./controllers/addProductHandler")
+const axios = require("axios");
 const cors = require("cors")
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -26,24 +27,89 @@ async function main() {
 
 
 app.get("/", async (req, res) => {
-
-    console.log(req.query)
-
-    addProductHandler(req, res)
-    const allProducts = await ProductDetails.find()
-    // res.json(allProducts)
-    res.send("index")
+  
+  console.log(req.query)
+  
+  addProductHandler(req, res)
+  const allProducts = await ProductDetails.find()
+  // res.json(allProducts)
+  res.send("index")
 });
 
 app.get("/p", async (req, res) => {
+  
+  console.log(req.query)
+  
+  addProductHandler(req, res)
+  const allProducts = await ProductDetails.find()
+  res.json(allProducts)
+});
 
-    console.log(req.query)
+app.get("/s", async (req, res) => {
+  try {
 
-    addProductHandler(req, res)
-    const allProducts = await ProductDetails.find()
-    res.json(allProducts)
+    // FIRST TRY
+    const response = await axios.get("https://www.gtinsearch.org/api/items/"+ req.query.code);
+    const data = response.data;
+
+    if(data[0]){
+      console.log("GOT IN 1ST TRY")
+      res.json(data[0]);
+    }
+    else{
+
+      // SECOND TRY
+      const settings = {
+        url: "https://api.upcdatabase.org/product/"+req.query.code,
+        method: "GET",
+        timeout: 0,
+        headers: {
+          Authorization: "Bearer THISISALIVEDEMOAPIKEY19651D54X47",
+        },
+      };
+      
+      axios(settings)
+        .then((response) => {
+
+          if(response.data.success){
+            console.log("GOT IN 2nd TRY")
+            res.send(response.data)
+          }
+          else{
+
+            // THIRD TRY
+            const responseData = thirdTry(req.query.code,req,res)
+            
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching data 2nd:", error.response.data);
+        });
+    }
+    
+
+
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on ${PORT}`);
 });
+
+
+
+
+
+  async function thirdTry (code,req,res){
+  const response = await axios.get(" https://www.brocade.io/api/items/"+ code);
+    const data = response.data;
+
+    if(data){
+      console.log("GOT IN 3rd TRY")
+      res.send(data)
+    }
+    else{}
+}
