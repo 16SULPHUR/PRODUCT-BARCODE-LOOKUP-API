ded50734522278102bb03abd8ba0b117639143b6
“Ankit <22amtics124@gmail.com>
2023-12-06 13:57:38 +0530

    bug

--- controllers/addDetails.js
+++ controllers/addDetails.js
@@ -10,16 +10,35 @@ async function addDetails(req, res) {
   let product = await ProductDetails.findOne({ code: body.code });
 
   if (product) {
+
     console.log(product);
 
-    const result = await ProductDetails.findByIdAndUpdate(
+    // const url =
+    //   "https://firebasestorage.googleapis.com/v0/b/android-project3-6c2f4.appspot.com/o/productImage%2F998866%2F2";
+
+    // try {
+    //   const response = await fetch(url);
+
+    //   if (!response.ok) {
+    //     throw new Error(`HTTP error! Status: ${response.status}`);
+    //   }
+
+    //   const data = await response.json();
+    //   console.log(data);
+    // } catch (error) {
+    //   console.error("Fetch error:", error.message);
+    // }
+
+    // console.log(result);
+
+    await ProductDetails.findByIdAndUpdate(
       { _id: product._id },
       {
         $setOnInsert: {
           "data.ratings": [],
           "data.reviewes": [],
           sustainabilityInfo: {
-            ratings: [],
+            ratings: [44],
             recyclability: [],
             recyclabilityDetails: [],
             energyEfficiencyDetails: [],
@@ -34,18 +53,18 @@ async function addDetails(req, res) {
       { _id: product._id },
       {
         $push: {
-          "data.title": body.title,
-          "data.description": body.description,
-          "data.images": { $each: body.images },
-          "data.ratings": body.ratings,
-          "data.reviews": body.review,
-          "data.sustainabilityInfo.ratings": body.sustainabilityInfo.ratings,
+          "data.title": body?.title || null,
+          "data.description": body?.description || null,
+          // "data.images": { $each: body?.images || null },
+          "data.ratings": body?.ratings || null,
+          "data.reviews": body?.review || null,
+          "data.sustainabilityInfo.ratings": body?.sustainabilityInfo?.ratings || null,
           "data.sustainabilityInfo.productRecyclability":
-            body.sustainabilityInfo.productRecyclability,
+            body?.sustainabilityInfo?.productRecyclability || null,
           "data.sustainabilityInfo.recyclabilityDetails":
-            body.sustainabilityInfo.recyclabilityDetails,
+            body?.sustainabilityInfo?.recyclabilityDetails || null,
           "data.sustainabilityInfo.packingDetails":
-            body.sustainabilityInfo.packingDetails,
+            body?.sustainabilityInfo?.packingDetails || null,
         },
       }
     );
